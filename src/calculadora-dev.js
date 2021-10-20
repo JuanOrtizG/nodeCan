bb122c215a5ea780567fbfe25268867a2566bb56
Juan <juanrodrigortiz@gmail.com>
2021-10-20 07:29:48 -0300

    formulas para graficar

---
+++ src/calculadora-dev.js
@@ -0,0 +1,92 @@
+//socket
+
+const socket = io()
+
+  
+  //REFERENCIAMOS LOS ELEMENTOS
+  let btn_activar     = document.getElementById('btn-activar')
+  let btn_desactivar  = document.getElementById('btn-desactivar')
+  let tabla           = document.getElementById('tablabody')
+  let input_formula   = document.getElementById('formula')
+  
+  
+  btn_activar.addEventListener('click', (event)=>{
+    event.preventDefault()
+    console.log("click")
+    
+    let senal = "i"+entrada.value+"t"+"z"
+    //let senal = "i" + "0c"+"t"+"z"
+    //AL HACER CLICK -> EMITIMOS la  SOCKET el codigo para leer datos rpm
+    console.log("emitimos: " + senal)
+      socket.emit('message', senal)
+  })
+ let cnt =0
+ 
+
+//CONFIGURACION DE CHART
+var ctx = document.getElementById('myChart').getContext('2d');
+
+var chart = new Chart(ctx, {
+  // The type of chart we want to create
+  type: 'line',
+
+  // The data for our dataset
+  data: {
+      labels: ["Serial"],
+      datasets: [{
+          label: "Datos de velocidad J1939",
+          backgroundColor: 'rgb(0, 123, 255)',
+          //#000000
+          borderColor: 'rgb(41, 128, 185)',
+          data: [],
+      }]
+  },
+
+  // Configuration options go here
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+  }
+});
+
+//FIN DE CONFIGURACION DE CHART
+
+// DATOS DEL PUERTO SERIAL
+let counter=0
+  socket.on('lectura',(data)=>{
+    console.log(data)
+    cnt++
+
+    chart.data.labels.push(counter);
+        chart.data.datasets.forEach(dataset => {
+          
+          //conversion a decimal
+          let A = parseInt(data.A,16)
+          let B = parseInt(data.B,16)
+          let C = parseInt(data.C,16)
+          let D = parseInt(data.D,16)
+          calculo = eval(input_formula.value)
+          console.log(calculo)
+          //conversionDecimal = parseInt(B, 16);
+          //dataset.data.push(conversionDecimal);
+          dataset.data.push(calculo)
+        });
+        counter++;
+        chart.update();
+ 
+
+  
+    
+  })
+// FIN DE LECTURA DEL PUERTO SERIAL
+
+
+  //BORRAR  CONTENIDO DE TEXTAREA
+  btn_desactivar.addEventListener('click', (event)=>{
+    event.preventDefault()
+    textarea.value = ""
+  })
+
+
+
+ 
